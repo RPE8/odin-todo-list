953338fb181c760508de5c1fa9bb67322cc1c84c
RPE8 <vitali.shuman96@gmail.com>
2022-09-17 16:02:29 +0300

    Add header logo text

--- src/header/header.ts
+++ src/header/header.ts
@@ -5,9 +5,10 @@ export const render = () => {
 	const header = createElement("header", [], ["id", "header"]);
 
 	const container = createElement("div", ["header__container", "container"]);
-	const logo = createIcon("list_alt", "md-36");
-
-	container.append(logo);
+	const logo = createIcon("list_alt", "md-36", "header__logo");
+	const logoText = createElement("span", "header__text");
+	logoText.textContent = "Todo List";
+	container.append(logo, logoText);
 	header.append(container);
 	return header;
 }

3878b4dad2e7ba6210557c3a6bbb5246fa2181c9
RPE8 <vitali.shuman96@gmail.com>
2022-09-17 14:15:46 +0300

    Add initial logic for creating content container

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {format} from 'date-fns';
+
+const body = document.querySelector("body");
+
+function createContentContainer(parent: HTMLBodyElement): HTMLDivElement {
+	const content = document.createElement("div");
+	content.setAttribute("id", "content");
+
+	parent.append(content);
+	return content;
+}
+
+if (!body) {
+	console.error("There is no parent");
+	throw new Error("There is no parent");
+}
+
+const content = createContentContainer(body);

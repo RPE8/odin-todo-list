3b50ed63464381892be9ea67b2501d322ceccd8f
RPE8 <vitali.shuman96@gmail.com>
2022-09-19 11:47:49 +0300

    Add initial aside toolbar structure

--- src/parts/aside/aside.ts
+++ src/parts/aside/aside.ts
@@ -4,6 +4,15 @@ import {createElement} from "../../utils";
 export const render = () => {
 	const container = createElement("div", ["aside__container", "container"])
 	const html = `<aside class="aside">
+		<menu class="aside__main-menu main-menu menu">
+			<li>Inbox</li>
+			<li>Today</li>
+			<li>This Week<li>
+		</menu>
+		<h3 class="aside__projects-title projects-title">Projects</h3>
+		<menu class="aside__projects-menu projects-menu menu" >
+			<li>Add Project<li>
+		</menu>
 	</aside>`;
 
 	container.innerHTML = html;
